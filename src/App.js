aac9b3c29a7f510d43f44e2e5e4dac2e6f9d2c1b
Your Name <you@example.com>
2021-10-10 17:56:22 +0300

    Tasks 1-3

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import Home from "./components/Home";
 // Components
 import ProductList from "./components/ProductList";
 //styling
-import "./App.css"
-import Detail from './components/Detail'
-
+import "./App.css";
+import Detail from "./components/Detail";
+import { Route, Switch } from "react-router";
+import NavBar from "./components/NavBar";
 
 function App() {
-
   return (
-    <div >
-      <Home />
-      <ProductList />
-      <Detail />
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/Productlist">
+          <ProductList />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
+// when exact path for home is "Home" I get an empty page. Why? Even with switch!
+// Attributes from routes = <></>
