93fe608ac7f032fe96144f3c174e2194ae149a0a
Your Name <you@example.com>
2021-10-11 15:58:38 +0300

    complete all tasks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div>
+    <div className="navBar">
       <Link to="/">
-        <h1>Home</h1>
+        <img src="https://uploads.turbologo.com/uploads/design/hq_preview_image/965291/draw_svg20210507-22909-14tgwma.svg.png" />
       </Link>
       <Link to="/ProductList">
         <h1>Products</h1>
